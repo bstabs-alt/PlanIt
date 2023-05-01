92c4c5aaaf92e2af3c615fce3807a650883370af
Reuben Sweetman <65689588+bstabs-alt@users.noreply.github.com>
2023-05-01 19:57:31 +1200

    Refactored to appropriate structure

---
+++ PlanIt.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PlanIt.Models;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        TaskManager taskManager = new TaskManager();
+
+        string title = GetUserInput("Enter task title:");
+        string description = GetUserInput("Enter task description:");
+
+        Console.WriteLine("Enter task due date (yyyy-mm-dd):");
+        DateTime dueDate;
+        if (!DateTime.TryParse(Console.ReadLine(), out dueDate))
+        {
+            Console.WriteLine("Invalid date format. Using today's date as default.");
+            dueDate = DateTime.Now;
+        }
+
+        PlannerTask newTask = taskManager.CreateTask(title, description, dueDate);
+
+        Console.WriteLine($"Task created: {newTask.Title} (ID: {newTask.Id})");
+    }
+
+    private static string GetUserInput(string prompt)
+    {
+        Console.WriteLine(prompt);
+        return Console.ReadLine() ?? string.Empty;
+    }
+}
