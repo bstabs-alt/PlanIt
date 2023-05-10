033ee954283d0521770d2aef44c5f80cbc3b456c
bstabs-alt <reubensweetmangough@gmail.com>
2023-05-10 12:32:08 +1200

    removed submodule

--- PlanIt/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if "%1" == "test" (
-  dotnet test PlanIt.Tests.sln
-) else if "%1" == "app" (
-  dotnet run --project PlanIt.ConsoleApp\PlanIt.ConsoleApp.csproj
-) else (
-  echo Usage: run.bat [test|app]
-)
